8d310bcfb11bea71e9d2cd397cec5b716ad06b9d
Krish <krijoshi@cisco.com>
2018-11-26 00:26:38 +0000

    Updates

---
+++ precache-manifest.3d957c45a1926074644d9500691b7c8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be0e18115774142a403b",
+    "url": "/stories/static/js/runtime~main.be0e1811.js"
+  },
+  {
+    "revision": "5225733742e908ed2ff8",
+    "url": "/stories/static/js/main.52257337.chunk.js"
+  },
+  {
+    "revision": "dd54cbdc3be3da309102",
+    "url": "/stories/static/js/1.dd54cbdc.chunk.js"
+  },
+  {
+    "revision": "5225733742e908ed2ff8",
+    "url": "/stories/static/css/main.79a343cf.chunk.css"
+  },
+  {
+    "revision": "9d203515412328027201374f2d2b983a",
+    "url": "/stories/index.html"
+  }
+];
